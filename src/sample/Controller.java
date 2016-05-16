6643b125f9d63b7346070209e520ee19ab79e925
xyntherys <azmainadel47@gmail.com>
2016-05-16 10:50:00 +0600

    Login Added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,17 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class Controller {
+public class Controller{
+
+    //ObservableList<String> GenderChoiceList = FXCollections.observableArrayList("Male","Female","Other");
+
     @FXML
     private Label lblstatus;
     @FXML
@@ -19,5 +25,15 @@ public class Controller {
         }
         else lblstatus.setText("Login Failed");
     }
+
+    /*@FXML
+    private ChoiceBox GenderChoiceBox;
+
+    @FXML
+    private void initialize(){
+        GenderChoiceBox.setValue("Male");
+        GenderChoiceBox.setItems(GenderChoiceList);
+
+    }*/
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Ticket Reservation System Demo");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
