fdac79c1ee24bfa85c0a8729116bbd0da6b65eec
xyntherys <azmainadel47@gmail.com>
2016-05-16 09:46:14 +0600

    Login Added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private Label lblstatus;
+    @FXML
+    private TextField userid;
+    @FXML
+    private TextField userpassword;
+
+    public void Login(ActionEvent event){
+        if(userid.getText().equals("azmain") && userpassword.getText().equals("pass")){
+            lblstatus.setText("Login Successful");
+        }
+        else lblstatus.setText("Login Failed");
+    }
+
 }
